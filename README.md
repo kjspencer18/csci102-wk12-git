8e8052b40803e53fda447772f0043a60b3a8b33f
Kate Spencer <kjspencer@mymail.mines.edu>
2019-11-19 11:18:39 -0700

    Created program that prints fibonacci sequence

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # csci102-wk12-git
+
+Kate Spencer, 11/19/2019, Git Workshop Week 2
---
+++ fib.py
@@ -0,0 +1,16 @@
+# fib.py
+
+def fib():
+    fibs = [1, 2]
+
+    for i in range(1,9):
+        num = fibs[i] + fibs[i-1]
+        fibs.append(num)
+
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
